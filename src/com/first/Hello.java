95ee574de03c1eb0539740d19486ebd33b5c6357
lumii <kerttu.lumi@gmail.com>
2018-09-14 14:41:59 +0300

    Hello

--- src/com/first/Hello.java
+++ src/com/first/Hello.java
@@ -1,5 +1,7 @@
+package com.first;
+
 public class Hello {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
 
         System.out.println("Hello Kerttu!");
 
