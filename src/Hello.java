3eead3ef3a19cbee7b90feeed613ddf65d587c7e
lumii <kerttu.lumi@gmail.com>
2018-09-14 14:32:09 +0300

    int myFirstNumber

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,10 @@
 public class Hello {
     public static void main (String[] args) {
+
         System.out.println("Hello Kerttu!");
+
+        int myFirstNumber = 10 + 5;
+        System.out.println(myFirstNumber);
+        System.out.println("myFirstNumber");
     }
 }
